33c4ed969254c05e538bde226d1099e5f3c40027
Jason <ceocjy@vip.qq.com>
2018-04-13 00:46:18 +0800

    Test: test filter

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+const url = require('url')
+
 module.exports = {
     summary: 'Hack Zhihu App api request',
     *beforeSendResponse(requestDetail, responseDetail) {
-        console.log(requestDetail.url)
+        const urlObj = url.parse(requestDetail.url)
+
+        // grep out zhihu feed
+        if (urlObj.hostname === 'api.zhihu.com' && urlObj.pathname === '/topstory/recommend') {
+            const newResponse = responseDetail.response;
+            const rawBody = newResponse.body
+            let body
+
+            try {
+                body = JSON.parse(rawBody)
+            } catch (e) {
+                console.log(e)
+                return
+            }
+        
+            const feed = body.data.filter(item => item.type === 'feed')
+            body,data = feed
+
+            return { response: newResponse }
+        }
     },
 };
